c7c0f7b0f4dfb382fc03f4abd43a89a3df346be8
Johnamehgreg <88066580+Johnamehgreg@users.noreply.github.com>
2023-04-17 02:38:22 -0700

    pdf

--- components/profile/ProfileTopInfo.vue
+++ components/profile/ProfileTopInfo.vue
@@ -3,70 +3,49 @@
     <!-- logo container -->
     <div class="bg-white md:w-[250px] w-full profile-image-container !py-0">
       <div class="icon-img md:mx-7 my-7 relative" @click="showImageCropperModal">
-        <img v-if="profilePhoto != null" :src="profilePhoto" id="referenceImg"
-          class="top-profile-image cursor-pointer" />
+        <img v-if="profilePhoto != null" :src="profilePhoto" id="referenceImg" class="top-profile-image cursor-pointer" />
         <span v-else>
           {{ firstCompanyName }}
         </span>
-        <div
-          v-if="isUser"
-          @click="visibleUploadImageDialog = true"
-          class="w-auto absolute bottom-3 right-3"
-        >
+        <div v-if="isUser" @click="visibleUploadImageDialog = true" class="w-auto absolute bottom-3 right-3">
           <upload-file-icon />
         </div>
       </div>
 
-      <cropper-image-upload
-        :show="visibleUploadImageDialog"
-        @visibleDialog="(show) => (visibleUploadImageDialog = show)"
-      />
+      <cropper-image-upload :show="visibleUploadImageDialog"
+        @visibleDialog="(show) => (visibleUploadImageDialog = show)" />
 
     </div>
     <!-- end of logo container -->
     <!-- dentals container -->
     <div class="bg-white sm:w-12/12 profile-dental-container">
       <!-- <h1>{{user.companyName || ''}}</h1> -->
-      <header class="text-600 text-[#414142] font-semibold pl-7 pb-9 border-b border-[#DCDCDC] relative">
-          <div class="input-wrapper-title flex justify-between items-start">
-        <!-- <input type="text" v-model="name" placeholder="Apple Dental" :disabled="true"
+      <header class="text-600 text-[#414142] font-semibold pl-7 pb-2 border-b border-[#DCDCDC] relative">
+
+        <div class="input-wrapper-title flex justify-between items-start">
+          <!-- <input type="text" v-model="name" placeholder="Apple Dental" :disabled="true"
           class="text-black text-2xl" /> -->
           <span class="text-2xl text-grey pl-3"> {{ name }}</span>
-          <button v-if="isUser" @click="toggleInput" class="float-right pr-10 md:pr-20">
-            <img
-              src="../../assets/img/pencil.png"
-              class="cursor-pointer w-[20px] h-[20px]"/>
-          </button>
+          <share-outline-icon :width="18" class="w-auto  pr-3" />
+          <!-- <button v-if="isUser" @click="toggleInput" class="float-right pr-10 md:pr-20">
+            <img src="../../assets/img/pencil.png" class="cursor-pointer w-[20px] h-[20px]" />
+          </button> -->
+          
         </div>
-        <share-outline-icon :width="18" class="w-auto absolute bottom-5 right-4 mr-px" />
+
+        
+       
       </header>
       <!--<div class="text-sm px-2 border-b w-full py-2 text-gray-400"><i>@hookname</i></div>-->
-      <div
-        v-if="!address && !phone && !isUser"
-        class="flex justify-center items-end h-28"
-      >
-        <p class="text-center font-medium">
+      <div class="flex justify-center items-end h-28">
+        <p class="text-center leading-8">
           We are doing our part to reduce carbon footprint. <br />
           Join us, complete our files on Paperdaz!
         </p>
       </div>
-      <div v-else>
-        <div class="px-8 py-3 border-b border-[#F0F0F0] input-wrapper">
-          <input type="text" v-model="address" placeholder="Company's address" :disabled="editEnalble"
-            :class="[editEnalble ? 'text-gray-400' : 'text-black']" />
-          <!-- <button v-if="isUser" @click="toggleInput">
-            <Pencil v-if="initialInput" :width="18" />
-            <span class="text-[15px] font-[900] text-paperdazgreen-500" v-else>&#x2715;</span>
-          </button> -->
-          </div>
-        <div class="px-8 py-3 input-wrapper">
-          <input type="number" v-model="phone" placeholder="Company's phone number" :disabled="editEnalble"
-            :class="[editEnalble ? 'text-gray-400' : 'text-black']" />
-          <!-- <button v-if="isUser" @click="toggleInput2">
-            <Pencil v-if="initialInput2" :width="18" />
-            <span class="text-[15px] font-[900] text-paperdazgreen-500" v-else>&#x2715;</span>
-          </button> -->
-        </div>
+      <!-- <div v-else>
+       
+     
         <div class="w-full grid place-items-center">
           <button
             class="w-[160px] flex justify-center items-center text-white py-2 mt-3 text-center border-none bg-paperdazgreen-400 rounded-md"
@@ -76,14 +55,14 @@
             <SpinnerDottedIcon v-if="isLoading" height="20" width="20" class="animate-spin ml-2" />
           </button>
         </div>
-      </div>
+
+
+      </div> -->
     </div>
     <!-- end of dentals container -->
   </section>
 </template>
-<style src="~/assets/cropper.css">
-
-</style>
+<style src="~/assets/cropper.css"></style>
 <script>
 import Pencil from '~/assets/recent-icons/pencil.vue'
 import Vue from 'vue'
@@ -134,9 +113,9 @@ export default mixins(login).extend({
     ballloader,
     UploadFileIcon,
     ShareOutlineIcon
- },
+  },
   methods: {
-    showImageCropperModal(){
+    showImageCropperModal() {
       this.isCreator ? this.visibleUploadImageDialog = true : null
     },
     getTeamPublicFolder() {
@@ -194,8 +173,8 @@ export default mixins(login).extend({
     //     },
   },
   computed: {
-    isCreator(){
-      return this.user?.id == this.userInfo?.id 
+    isCreator() {
+      return this.user?.id == this.userInfo?.id
     },
     firstCompanyName() {
       return (this.userInfo?.companyName || '').charAt(0).toUpperCase()
@@ -216,7 +195,7 @@ export default mixins(login).extend({
       return this.user?.id == this.userInfo?.id
     },
     profilePhoto() {
-      return  this.userInfo?.profilePicture
+      return this.userInfo?.profilePicture
     },
   },
   watch: {
@@ -225,7 +204,7 @@ export default mixins(login).extend({
       this.phone = this.$auth.user?.phone || ''
       // QRCode.toCanvas(this.$refs.qrcancas, this.qrCodeurl, function () {});
     },
-    userInfo: function() {
+    userInfo: function () {
       this.phone = this.userInfo?.phone
       this.address = this.userInfo?.address
       this.name = this.userInfo?.companyName
@@ -242,7 +221,7 @@ export default mixins(login).extend({
   @apply bg-white flex justify-center flex-wrap items-center py-4 rounded-[20px];
 
   .icon-img {
-    @apply w-[195px] h-[195px] font-[900] text-[6em] text-white cursor-pointer  bg-[#77B550] grid place-items-center rounded-[30px];
+    @apply w-[195px] h-[195px] font-[900] text-[6em] text-white cursor-pointer bg-[#77B550] grid place-items-center rounded-[30px];
     // text-shadow: 1px 5px 7px rgb(148 148 148);
   }
 
--- layouts/profile.vue
+++ layouts/profile.vue
@@ -9,7 +9,7 @@
       <div class="overlay" @click="collapseSidebar = false"></div>
       <ProfileSidebar class="relative" @close="collapseSidebar = false" />
     </div>
-     <ProfileTab/>
+     <!-- <ProfileTab/> -->
 
     <div id="main-container" class="overflow-y-auto overflow-x-hidden">
       <Nuxt class="py-4 flex-1 px-3 px-lg-0 lg:pb-10" />

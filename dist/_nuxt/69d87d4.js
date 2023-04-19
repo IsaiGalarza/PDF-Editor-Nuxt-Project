4dfbb3912feb0f20b40d47149f2fc82d82e7c2b6
IsaiGalarza <isai.galarza@gmail.com>
2023-04-19 15:12:11 +0100

    error commit

--- dist/_nuxt/69d87d4.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see LICENSES */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[305],{1945:function(e,n,r){"use strict";r.r(n);var o=r(1338);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","9.15.0","app-compat")}}]);

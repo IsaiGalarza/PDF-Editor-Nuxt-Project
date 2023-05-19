84d9028fdd55d3f4ad49c63a91fcc6291401b1c9
IsaiGalarza <isai.galarza@gmail.com>
2023-05-19 09:19:49 +0100

    restructure

--- components/pdf/modals/PublishPdfModal.vue
+++ components/pdf/modals/PublishPdfModal.vue
@@ -483,12 +483,12 @@ export default mixins(SaveSignatureInitialsMixin).extend({
             msg: `Thank you for completing a Paperlink!`,
             msg_mobile: 'Thank You'
           })
-          // this.$store.commit('SET_FILE_SIGNATURE', null)
-          // this.$store.commit('SET_FILE_INITIAL', null)
-          // this.$store.commit("UN_SET_AGREE_SIGN")
-          // this.$auth.loggedIn && this.isCreator
-          //   ? this.$nuxt.$router.push('/paperlink-pages')
-          //   : this.$nuxt.$router.push(`/${this.file?.user?.businessPage}`)
+          this.$store.commit('SET_FILE_SIGNATURE', null)
+          this.$store.commit('SET_FILE_INITIAL', null)
+          this.$store.commit("UN_SET_AGREE_SIGN")
+          this.$auth.loggedIn && this.isCreator
+            ? this.$nuxt.$router.push('/paperlink-pages')
+            : this.$nuxt.$router.push(`/${this.file?.user?.businessPage}`)
         })
       } catch (error) {
         this.$notify.error({

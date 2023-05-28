d8dbf66f24d14880cc2467cb7dac7a2f56dedadb
IsaiGalarza <isai.galarza@gmail.com>
2023-05-28 06:58:29 +0100

    places readjusted in pdf

--- components/pdf/modals/PublishPdfModal.vue
+++ components/pdf/modals/PublishPdfModal.vue
@@ -454,8 +454,8 @@ export default mixins(SaveSignatureInitialsMixin).extend({
           // actionBy: `${this.$auth.user.firstName} ${this.$auth.user.lastName}`.toUpperCase()
         })
         .then((response) => {
-          // this.isLoading = false
-          this.addToLedger()
+          this.isLoading = false
+          // this.addToLedger()
           this.generatedPdf = response.data
         })
       } catch (error) {
